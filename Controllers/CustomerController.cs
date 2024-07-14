4a58c512dd56070d3f0db705b227a137e5a9bd8d
aijazmaq <aijazmaq@gmail.com>
2024-07-14 12:09:23 +0530

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Infrastructure.Model;
+using Service.Implementation;
+using Service.Interfaces;
+
+namespace CustomerManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomer _icustomer; 
+        public CustomerController(ICustomer customer) 
+        {
+            _icustomer = customer;
+        }
+
+        [HttpGet("GetCustomer")]
+        public CustomerResponse GetCustomer()
+        {
+            return _icustomer.GetCustomer();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.OpenApi.Models;
+using Service.Implementation;
+using Service.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Cutomer API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
+IServiceCollection services = builder.Services;
+//var Configuration = builder.Configuration;
+//services.AddSingleton<ILearningInterface, LearningService>();  //registration of services
+services.AddScoped<ICustomer, CustomerService>();
+//services.AddDbContext<DataContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseRouting();
+
+app.Run();
